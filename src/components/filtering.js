097c239a68ed6f6a65b87c6a4dc3396da3b0148b
Nikita <nikitakonovalov349@mail.ru>
2025-12-07 00:09:21 +0700

    function (smart table): transfer all data operations to the server (paging, filtering, sorting, and searching).

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,38 +1,35 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
-
-// @todo: #4.3 — настроить компаратор
-const compare = createComparison(defaultRules); 
-export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
-    Object.keys(indexes)                                    // Получаем ключи из объекта
-      .forEach((elementName) => {                        // Перебираем по именам
-        elements[elementName].append(                    // в каждый элемент добавляем опции
-            ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
-                      .map(name => {                        // используйте name как значение и текстовое содержимое
-                                                        // @todo: создать и вернуть тег опции
-                        const option = document.createElement('option');
-                          option.value = name;
-                          option.textContent = name;
-                          return option;
-                      })
-        )
-     })
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
-        if (action && action.name === 'clear') {
-            const fieldToClear = action.dataset.field; // Получаем имя поля из data-field кнопки
+export function initFiltering(elements) {
+    const updateIndexes = (elements, indexes) => {
+        Object.keys(indexes).forEach((elementName) => {
+            elements[elementName].append(...Object.values(indexes[elementName]).map(name => {
+                const el = document.createElement('option');
+                el.textContent = name;
+                el.value = name;
+                return el;
+            }))
+        })
+    }
+    const applyFiltering = (query, state, action) => {
+        if (action?.element?.closest('[data-clear]')) {
+            const clearBtn = action.element.closest('[data-clear]');
+            const fieldToClear = clearBtn.dataset.field;
             if (elements[fieldToClear]) {
-                // Находим родительский элемент кнопки
-                const parentElement = action.parentElement;
-                // Ищем input внутри родителя
-                const inputToClear = parentElement.querySelector('input');
-                if (inputToClear) {
-                    inputToClear.value = ''; // Сбрасываем значение поля ввода
-                }
-
+                elements[fieldToClear].value = '';
+                state = { ...state, [fieldToClear]: '' };
             }
         }
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data.filter(row => compare(row, state)); 
+        const filter = {};
+        Object.keys(elements).forEach(key => {
+            if (elements[key]) {
+                if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) {
+                    filter[`filter[${elements[key].name}]`] = elements[key].value;
+                }
+            }
+        })
+        return Object.keys(filter).length ? Object.assign({}, query, filter) : query;
+    }
+    return {
+        updateIndexes,
+        applyFiltering
     }
 }
--- src/components/searching.js
+++ src/components/searching.js
@@ -1,20 +1,7 @@
-import {rules, createComparison} from "../lib/compare.js";
-
 export function initSearching(searchField) {
-
-    const searchComparator = createComparison(
-        [], // Пустой массив правил для обработки целевого значения в компараторе по умолчанию
-        rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false) // Правило для поиска
-    );
-
-    return (data, state, action) => {
-
-        const searchValue = state[searchField];
-
-        if (!searchValue) {
-
-            return data;
-        }
-        return data.filter(item => searchComparator(item, searchValue));
+    return (query, state, action) => {
+        return state[searchField] ? Object.assign({}, query, {
+            search: state[searchField]
+        }) : query;
     }
 }
